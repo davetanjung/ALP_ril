811581b83b6f622edbd9e22e1ea82d7370500c86
davetanjung <153162404+davetanjung@users.noreply.github.com>
2024-12-10 10:26:31 +0700

    added_use_factory

--- app/Models/Lecturers_Student.php
+++ app/Models/Lecturers_Student.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Lecturers_Student extends Model
 {
-    //
+    use HasFactory;
 }
