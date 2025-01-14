8e6de4f39609a6856a3929ce7823ff79a0999c47
davetanjung <153162404+davetanjung@users.noreply.github.com>
2025-01-14 23:06:03 +0700

    navigation responsive

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,15 +1,23 @@
 <div class="w-full flex justify-between items-center mb-8">
-    <a href="{{ route('home') }}">
-        <img src="{{ asset('/images/logoo.png') }}" alt="Logo" class="h-14 w-12">
-    </a>
-    <button id="menuToggle" class="md:hidden focus:outline-none">
-        <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="24" height="3" rx="1.5" fill="black" />
-            <rect y="6" width="24" height="3" rx="1.5" fill="black" />
-            <rect y="12" width="24" height="3" rx="1.5" fill="black" />
-        </svg>
+    <div class="flex w-full items-center justify-start md:hidden">
+        <a href="{{ route('home') }}" class="mr-4">
+            <img src="{{ asset('/images/logoo.png') }}" alt="Logo" class="h-16 w-14">
+        </a>
+        @auth
+            <div class="md:hidden">
+                <a href="{{ route('profile', Auth::user()->id) }}">
+                    <img src="{{ asset(Auth::user()->profile_image) }}" alt="User Profile"
+                        class="w-16 h-16 object-cover rounded-full border-[#232360] border">
+                </a>
+            </div>
+        @endauth
+    </div>
 
-    </button>
+    <a href="{{ route('home') }}" class="mr-4 hidden md:block">
+        <img src="{{ asset('/images/logoo.png') }}" alt="Logo" class="h-16 w-14">
+    </a>
+
+
     <nav id="menu" class="hidden md:flex md:w-full md:justify-center">
         <a href="{{ route('home') }}"
             class="px-8 text-black transition hover:text-black/70 focus:outline-none {{ Route::currentRouteName() == 'home' ? 'text-[#232360] font-bold' : 'text-[#232360] text-opacity-[0.42]' }}">
@@ -32,20 +40,28 @@
             Project
         </a>
         <a href="{{ route('credit') }}"
-        class="px-8 text-black transition hover:text-black/70 focus:outline-none {{ Route::currentRouteName() == 'credit' ? 'text-[#232360] font-bold' : 'text-[#232360] text-opacity-[0.42]' }}">
-        Credit
-    </a>
+            class="px-8 text-black transition hover:text-black/70 focus:outline-none {{ Route::currentRouteName() == 'credit' ? 'text-[#232360] font-bold' : 'text-[#232360] text-opacity-[0.42]' }}">
+            Credit
+        </a>
     </nav>
-    @auth        
-        <div>
+
+    @auth
+        <div class="hidden md:block">
             <a href="{{ route('profile', Auth::user()->id) }}">
                 <img src="{{ asset(Auth::user()->profile_image) }}" alt="User Profile"
-                class="rounded-full border-2 border-[#AA98CA] w-16 h-16">
-            </a>            
+                    class="w-16 h-16 object-cover rounded-full border-[#232360] border">
+            </a>
         </div>
     @endauth
+
+    <button id="menuToggle" class="md:hidden focus:outline-none">
+        <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="24" height="3" rx="1.5" fill="black" />
+            <rect y="6" width="24" height="3" rx="1.5" fill="black" />
+            <rect y="12" width="24" height="3" rx="1.5" fill="black" />
+        </svg>
+    </button>
     @guest
-        <!-- Guest user will see this -->
         <a href="{{ route('login') }}"
             class="border-2 px-8 py-1 text-black transition hover:text-black/70 focus:outline-none rounded-full">Login</a>
         <a href="{{ route('register') }}"
@@ -74,8 +90,8 @@
             Project
         </a>
         <a href="{{ route('credit') }}"
-    class="px-8 text-black transition hover:text-black/70 focus:outline-none {{ Route::currentRouteName() == 'credit' ? 'text-[#232360] font-bold' : 'text-[#232360] text-opacity-[0.42]' }}">
-    Credit
-</a>
+            class="py-2 text-black transition hover:text-black/70 focus:outline-none {{ Route::currentRouteName() == 'credit' ? 'text-[#232360] font-bold' : 'text-[#232360] text-opacity-[0.42]' }}">
+            Credit
+        </a>
     </div>
 </div>
